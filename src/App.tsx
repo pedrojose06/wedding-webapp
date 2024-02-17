c77c185f27d6761f6c6863c1662466cdeac1935c
Pedro Moraes <esapiens@C0513.local>
2024-02-17 16:27:41 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { atom, useAtom } from 'jotai';
+
+const counter = atom(0);
+
+export default function App() {
+  const [count, setCounter] = useAtom(counter);
+  const onClick = () => setCounter(prev => prev + 1);
+  return (
+    <div>
+       <h1>{count}</h1>
+      <button onClick={onClick}>Click</button>
+    </div>
+  )
+}
